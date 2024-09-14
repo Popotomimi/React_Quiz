5b3c8507ca185d6512998626e7d8b85d9a67f8a5
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-09-14 11:36:19 -0300

    Responsividade
    Adicionado a responsividade do projeto e o navbar e footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@
 import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
+// React Router Dom
+import { BrowserRouter as Router } from "react-router-dom";
+
 // Components
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
 import PickCategory from "./components/PickCategory";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 // CSS
 import "./App.css";
@@ -15,12 +20,18 @@ function App() {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="App">
-      <h1>Quiz de Programação</h1>
-      {quizState.gameStage === "Start" && <Welcome />}
-      {quizState.gameStage === "Category" && <PickCategory />}
-      {quizState.gameStage === "Playing" && <Question />}
-      {quizState.gameStage === "End" && <GameOver />}
+    <div>
+      <Router>
+        <Navbar />
+        <div className="App">
+          <h1>Quiz de Programação</h1>
+          {quizState.gameStage === "Start" && <Welcome />}
+          {quizState.gameStage === "Category" && <PickCategory />}
+          {quizState.gameStage === "Playing" && <Question />}
+          {quizState.gameStage === "End" && <GameOver />}
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>React Quiz</h3>
+    </nav>
+  );
+};
+
+export default Navbar;
